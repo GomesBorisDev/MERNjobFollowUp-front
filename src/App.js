8b904dbb724c15ee78f5699ed9a976def9f2e299
GomesBorisDev <gomesboris.dev@gmail.com>
2020-08-03 20:27:37 +0200

    react-router-dom + nav + applications page

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import './App.css';
 import Nav from './components/Nav';
 import AddAppsForm from './components/AddAppsForm';
 import Applications from './components/Applications';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       {/* <header className="App-header">
       </header> */}
+      <Router>
+        <Nav/>
+        <h1>My Job Applications !</h1>
 
-      <Nav/>
+        <Switch>
+          <Route exact path="/" component={AddAppsForm}></Route>
+          <Route path="/applications" component={Applications}></Route>   
+        </Switch>
 
-      <h1>My Job Applications !</h1>
-
-      <AddAppsForm/>
-
-      <div>
-        <Applications/>
-      </div>
-
+    </Router>
     </div>
   );
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
     
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <Link to="/" className="navbar-brand">My Job Apps</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -12,7 +13,8 @@ const Nav = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">My Applications<span className="sr-only">(current)</span></a>
+                        <Link to="/applications" className="nav-link">My Applications</Link>
+                        {/* <span className="sr-only">(current)</span> */}
                     </li>
                 </ul>
             </div>
